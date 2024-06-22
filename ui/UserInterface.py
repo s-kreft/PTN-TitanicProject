8f7785c8216f500fc5c8deadb76d2bf80d9e6269
s-kreft <sebastian.kreft.sg@gmail.com>
2024-06-22 20:19:02 +0200

    Program now works in loop

--- ui/UserInterface.py
+++ ui/UserInterface.py
@@ -12,25 +12,28 @@ class UserInterface:
               "Wyświetl wykres - wybierz 5\n"
               "Wyjdź z programu - wybierz q")
     def switch(self):
-        user_input = input("Wybierz akcję\n")
-        if user_input == "1":
-            self.service.titanic_data()
-            return print("Wybrałeś 1")
-        elif user_input == "2":
-            self.service.show_titanic_data()
-            return "Wybrałeś 2"
-        elif user_input == "3":
-            search_input = input("Podaj wartość\n")
-            self.service.search_titanic_data(search_input)
-            return "Wybrałeś 3"
-        elif user_input == "4":
-            search_input = input("Podaj wartość\n")
-            self.service.search_titanic_age(search_input)
-            return "Wybrałeś 4"
-        elif user_input == "5":
-            self.service.titanic_age_plot()
-            return "Wybrałeś 5"
-        elif user_input == "q":
-            return "Wyszedłeś z programu"
-        else:
-            return print("Akcja niedozwolona")
+        running = True
+        while running == True:
+            user_input = input("Wybierz akcję\n")
+            if user_input == "1":
+                self.service.titanic_data()
+                print("Wybrałeś 1")
+            elif user_input == "2":
+                self.service.show_titanic_data()
+                print("Wybrałeś 2")
+            elif user_input == "3":
+                search_input = input("Podaj wartość\n")
+                self.service.search_titanic_data(search_input)
+                print("Wybrałeś 3")
+            elif user_input == "4":
+                search_input = input("Podaj wartość\n")
+                self.service.search_titanic_age(search_input)
+                print("Wybrałeś 4")
+            elif user_input == "5":
+                self.service.titanic_age_plot()
+                print("Wybrałeś 5")
+            elif user_input == "q":
+                running = False
+                print("Wyszedłeś z programu")
+            else:
+                print("Akcja niedozwolona")
