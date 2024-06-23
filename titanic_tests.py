a29f9babb61c836bfc1bc4906134440199916405
s-kreft <sebastian.kreft.sg@gmail.com>
2024-06-23 18:20:53 +0200

    Add unit test test_search_age_is_lesser_than

--- titanic_tests.py
+++ titanic_tests.py
@@ -1,16 +1,30 @@
 import unittest
+from unittest.mock import patch
 from service.Service import Service
 import numpy as np
 import pandas as pd
+
+def get_user_input():
+    return input("<")
 
 class TitanicTests(unittest.TestCase):
     
     def test_search_titanic_data_returns_only_male(self):
         self.service = Service()
         user_input = 'm'
-        df = self.service.search_gender_titanic_data(user_input)
+        self.service.search_gender_titanic_data(user_input)
         contains_male = all(self.service.contemporary_data_frame['Płeć'].str.fullmatch('male'))
         self.assertTrue(contains_male)
+    
+    @patch('builtins.input', return_value='<')
+    def test_search_age_is_lesser_than(self, mocked_input):
+        self.service = Service()
+        user_input = '20'
+        self.service.search_titanic_age(user_input)
+        age_test = all(self.service.contemporary_data_frame['Wiek'] < 20)
+        self.assertTrue(age_test)
+
+
 
     # def test_quick(self):
     #     number_one = 2
