36b2dfc62e33ed25e802201b7d15778f7994e8c2
s-kreft <sebastian.kreft.sg@gmail.com>
2024-06-23 18:03:53 +0200

    Add unit test test_search_titanic_data_returns_only_male

--- titanic_tests.py
+++ titanic_tests.py
@@ -4,13 +4,18 @@ import numpy as np
 import pandas as pd
 
 class TitanicTests(unittest.TestCase):
-    def __init__(self, input):
-        self.service = Service()
     
-    def test_search_titanic_data_return_only_male(self):
-        df = self.service.search_male_titanic_data
-        contains_test = df['Płeć'].str.contains('male')
-        self.assertTrue(contains_test)
+    def test_search_titanic_data_returns_only_male(self):
+        self.service = Service()
+        user_input = 'm'
+        df = self.service.search_gender_titanic_data(user_input)
+        contains_male = all(self.service.contemporary_data_frame['Płeć'].str.fullmatch('male'))
+        self.assertTrue(contains_male)
+
+    # def test_quick(self):
+    #     number_one = 2
+    #     number_two = 2
+    #     self.assertEqual(number_one, number_two)
 
 if __name__ == '__main__':
     unittest.main()
